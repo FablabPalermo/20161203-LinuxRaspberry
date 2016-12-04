6e44888e3881314d35174b60a6672a51bb79d5a8
gionni <giovbat@gmail.com>
2016-12-04 06:16:48 +0100

    a bit of python

---
+++ blink.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# blink-led.py - Continuously blink an LED every second
+#
+
+import RPi.GPIO as GPIO
+import time
+
+LED = 22
+
+GPIO.setmode(GPIO.BCM)		# Set to board numbering
+GPIO.setup(LED, GPIO.OUT)	# Setup LED channel for output
+GPIO.output(LED, GPIO.LOW)	# Initialize output state to low
+
+try:
+  while True:
+	GPIO.output(LED, GPIO.HIGH)	# Turn on LED
+	time.sleep(1)
+	GPIO.output(LED, GPIO.LOW)	# Turn off LED
+	time.sleep(1)
+except KeyboardInterrupt:
+  print "Exiting"
+  GPIO.cleanup()
+
